14b6ee891a728dd59aade8b2beea6c0366a66152
Leon Scharnweber <git.hub.leon@gmail.com>
2026-02-19 10:04:20 +0100

    deleted unnecessary declaration of icons

--- init.lua
+++ init.lua
@@ -98,25 +98,11 @@ require 'config/lazy-bootstrap'
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup( 'plugins', {
+require('lazy').setup('plugins', {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
+    icons = vim.g.have_nerd_font and {},
   },
 })
 -- vim: ts=2 sts=2 sw=2 et
