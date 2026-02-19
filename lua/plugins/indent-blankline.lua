dd5b16efc009aedf487746faf1c908abc58caae9
Leon Scharnweber <git.hub.leon@gmail.com>
2026-02-19 11:54:05 +0100

    indent-blankline plugin configured

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,6 +4,23 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      enabled = true,
+      debounce = 200,
+      viewport_buffer = {
+        min = 30,
+      },
+      indent = {
+        char = '│',
+        tab_char = '│',
+      },
+      scope = {
+        show_exact_scope = true,
+        injected_languages = true,
+      },
+      exclude = {
+        filetypes = { 'markdown', 'lspinfo', 'packer', 'checkhealth', 'help', 'man', 'gitcommit', 'TelescopePrompt', 'TelescopeResults', '' },
+      },
+    },
   },
 }
