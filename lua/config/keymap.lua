22850cf8d48543573f4b85e318c3156390e22ac3
Leon Scharnweber <git.hub.leon@gmail.com>
2026-02-22 20:42:37 +0100

    added keymaps for closing and open neotree window

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -46,8 +46,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+--Neotree keymaps
+--
+vim.keymap.set('n', '<leader>no', '<cmd> Neotree<CR>', { desc = 'Open Neotree window' })
+vim.keymap.set('n', '<leader>nc', '<cmd> Neotree action=close<CR>', { desc = 'Close Neotree window' })
