7ccdc0ec9249a9a6bdc0f96a13858b014dd944b9
Leon Scharnweber <git.hub.leon@gmail.com>
2026-02-23 01:19:06 +0100

    change keymapping for completion to enter and tap

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -18,12 +18,10 @@ return { -- Autocompletion
         -- `friendly-snippets` contains a variety of premade snippets.
         --    See the README about individual language/framework/plugin snippets:
         --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
+        {
+          'rafamadriz/friendly-snippets',
+          config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
+        },
       },
       opts = {},
     },
@@ -32,31 +30,17 @@ return { -- Autocompletion
   --- @type blink.cmp.Config
   opts = {
     keymap = {
-      -- 'default' (recommended) for mappings similar to built-in completions
-      --   <c-y> to accept ([y]es) the completion.
-      --    This will auto-import if your LSP supports it.
-      --    This will expand snippets if the LSP sent a snippet.
-      -- 'super-tab' for tab to accept
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- For an understanding of why the 'default' preset is recommended,
-      -- you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      --
-      -- All presets have the following mappings:
-      -- <tab>/<s-tab>: move to right/left of your snippet expansion
-      -- <c-space>: Open menu or open docs if already open
-      -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-      -- <c-e>: Hide menu
-      -- <c-k>: Toggle signature help
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = 'default',
+      preset = 'none',
 
-      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-      --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      ['<CR>'] = { 'accept', 'fallback' },
+
+      ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+
+      ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+
+      ['<C-Space>'] = { 'show', 'show_documentation' },
+      ['<C-e>'] = { 'hide' },
+
     },
 
     appearance = {
