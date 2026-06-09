668d457a48cecd8b7261d63cc58393f7aa63cd8e
Leon Scharnweber <git.hub.leon@gmail.com>
2026-06-09 09:25:50 +0200

    put c to the formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -118,7 +118,7 @@ return {
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
     --  See `:help lsp-config` for information about keys and how to configure
     local servers = {
-      -- clangd = {},
+      clangd = {},
       -- gopls = {},
       -- pyright = {},
       -- rust_analyzer = {},
