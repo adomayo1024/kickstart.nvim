a7eda222b63b62d2f2644a6b80bf008a4e1a39a8
Leon Scharnwebwer <leon.scharnweber@gmail.com>
2026-07-28 13:14:25 +0200

    eslint_d bedingungen und gelöscht und prettiered zu prettier geändert

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,9 +10,7 @@ return {
 
     null_ls.setup {
       sources = {
-        require 'none-ls.diagnostics.eslint_d',
-        require 'none-ls.formatting.eslint_d',
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.prettier,
       },
 
       -- Format on save.
